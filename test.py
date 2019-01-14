f56c00aead012a768d71f290aa4c8a0c3261ca31
umihico <umihico@users.noreply.github.com>
2019-01-15 07:53:25 +0900

    version 0.0.9 figure out json error behavior

--- test.py
+++ test.py
@@ -24,15 +24,19 @@ def load_test(data):
         pickled_data = pickle.load(f)
     with open(filenames['json'], 'r', encoding='utf-8',) as f:
         jsoned_data = json.load(f)
+
+    print("origin data", data)
     print('ppickle', data == ppickled_data)
     print('pickle', data == pickled_data)
     print('json', data == jsoned_data)
+    if jsoned_data != data:
+        print(jsoned_data)  # json make it list instead of tuple?
 
 
 def test():
     testdata1 = {'ar': 'عربى', 'en': 'english', 'jp': '日本語', 'ko': '한국어'}
     testdata2 = list(testdata1.items())
-    testdata3 = collections.OrderedDict()  # fails
+    testdata3 = collections.OrderedDict()  # ppickle fails
     for k, v in testdata2:
         testdata3[k] = v
     testdatas = [testdata1, testdata2, testdata3]
