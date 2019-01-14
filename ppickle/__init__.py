6cfe8dd3a36ae8d28bc462bd6776c105348fb8de
umihico <umihico@users.noreply.github.com>
2019-01-14 20:16:57 +0900

    version 0.0.4 add error detect fucntion

--- ppickle/__init__.py
+++ ppickle/__init__.py
@@ -10,8 +10,13 @@ def dump(filename, data):
     with codecs.open(filename, 'w', 'utf-8') as f:
         f.write(pformat(data))
     """
+    stringed_data = pformat(data)
+    try:
+        ast.literal_eval(stringed_data)
+    except Exception as e:
+        raise Exception("given data is not evaluable by ast.literal_eval")
     with codecs.open(filename, 'w', 'utf-8') as f:
-        f.write(pformat(data))
+        f.write(stringed_data)
 
 
 def load(filename):
